296d20f64289b6e06f25a6207181d91e79e981c4
Brodie M Busby <93949221+brodiembusby@users.noreply.github.com>
2026-06-25 12:36:52 -0600

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+    
+    
+    # Brute Force solution     
+        for i in range(len(nums)):
+            for j in range(i +1,len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [min(i,j), max(i,j)]
+        return[0,0]
+
+         
